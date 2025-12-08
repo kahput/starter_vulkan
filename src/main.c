a3732b8389c57da257c222be4e049b1157f1ca74
kahput <kahputs@gmail.com>
2025-12-08 14:02:06 +0100

    feat(main): bundle model data and load two different models

--- src/main.c
+++ src/main.c
@@ -9,6 +9,7 @@
 #include "renderer/renderer_types.h"
 #include "renderer/vk_renderer.h"
 
+#include <assert.h>
 #include <cglm/affine-pre.h>
 #include <cglm/mat4.h>
 #include <cglm/util.h>
@@ -50,10 +51,21 @@ typedef struct camera {
 	mat4 view, projection;
 } Camera;
 
-#define MAX_MESHES 256
-#define MAX_MATERIAL_INSTANCES 32
-#define DEFAULT_TEXTURE_BASE_COLOR 0
-#define DEFAULT_TEXTURE_OFFSET DEFAULT_TEXTURE_BASE_COLOR + 1
+#define MAX_MODEL_MESHES 32
+#define MAX_MODEL_MATERIALS 8
+#define MAX_MODEL_TEXTURES 8
+#define MAX_MODELS 8
+
+typedef struct model {
+	Mesh meshes[MAX_MODEL_MESHES];
+	uint32_t mesh_count;
+
+	MaterialInstance materials[MAX_MODEL_MATERIALS];
+	uint32_t material_count;
+
+	Texture textures[MAX_MODEL_TEXTURES];
+	uint32_t texture_count;
+} Model;
 
 static struct State {
 	Arena *permanent, *frame, *assets;
@@ -66,19 +78,12 @@ static struct State {
 	uint32_t default_texture_base_color;
 	uint32_t default_texture_offset;
 
-	Mesh meshes[MAX_MESHES];
-	uint32_t mesh_count;
+	Model models[MAX_MODELS];
+	uint32_t model_count;
 
 	Material PBR_material;
 
-	MaterialInstance materials[MAX_MATERIAL_INSTANCES];
-	uint32_t material_count;
-
-	// GPU resources
-	Texture textures[MAX_TEXTURES];
-	uint32_t texture_count;
-
-	uint32_t buffer_count, set_count, sampler_count;
+	uint32_t buffer_count, texture_count, set_count, sampler_count;
 
 	uint64_t start_time;
 } state;
@@ -137,6 +142,10 @@ int main(void) {
 	SceneAsset *scene = importer_load_gltf(state.assets, MAGE_FILE_PATH);
 	upload_scene(scene);
 	arena_end_temp(temp);
+	temp = arena_begin_temp(state.assets);
+	scene = importer_load_gltf(state.assets, BOT_FILE_PATH);
+	upload_scene(scene);
+	arena_end_temp(temp);
 
 	state.camera = (Camera){
 		.speed = 3.0f,
@@ -166,21 +175,25 @@ int main(void) {
 		vulkan_renderer_bind_pipeline(state.ctx, 0);
 		vulkan_renderer_bind_resource_set(state.ctx, scene_set);
 
-		mat4 model_matrix;
-		glm_mat4_identity(model_matrix);
-		// glm_mat4_scale(model_matrix, 10);
-		vulkan_renderer_push_constants(state.ctx, 0, "push_constants", model_matrix);
+		for (uint32_t model_index = 0; model_index < state.model_count; ++model_index) {
+			Model *model = &state.models[model_index];
 
-		for (uint32_t mesh_index = 0; mesh_index < state.mesh_count; ++mesh_index) {
-			Mesh *mesh = &state.meshes[mesh_index];
-			MaterialInstance *material = &state.materials[mesh->material];
+			mat4 model_matrix;
+			glm_mat4_identity(model_matrix);
+			glm_translate(model_matrix, (vec3){ 2.0f * model_index, 0.0f, 0.0f });
+			vulkan_renderer_push_constants(state.ctx, 0, "push_constants", model_matrix);
 
-			vulkan_renderer_bind_resource_set(state.ctx, material->resource_set);
+			for (uint32_t mesh_index = 0; mesh_index < model->mesh_count; ++mesh_index) {
+				Mesh *mesh = &model->meshes[mesh_index];
+				MaterialInstance *material = &model->materials[mesh->material];
 
-			vulkan_renderer_bind_buffer(state.ctx, mesh->vertex_buffer);
-			vulkan_renderer_bind_buffer(state.ctx, mesh->index_buffer);
+				vulkan_renderer_bind_resource_set(state.ctx, material->resource_set);
 
-			vulkan_renderer_draw_indexed(state.ctx, mesh->index_count);
+				vulkan_renderer_bind_buffer(state.ctx, mesh->vertex_buffer);
+				vulkan_renderer_bind_buffer(state.ctx, mesh->index_buffer);
+
+				vulkan_renderer_draw_indexed(state.ctx, mesh->index_count);
+			}
 		}
 
 		Vulkan_renderer_end_frame(state.ctx);
@@ -201,75 +214,86 @@ int main(void) {
 }
 
 bool upload_scene(SceneAsset *scene) {
-	if (state.buffer_count >= MAX_MESHES) {
+	if (state.buffer_count >= MAX_MODEL_MESHES) {
 		LOG_WARN("Main: mesh slots filled, aborting upload");
 		return false;
 	}
 
+	uint32_t texture_offset = state.texture_count;
+	Model *dst = &state.models[state.model_count++];
+
 	// TODO: Make use of, and upload all relevant material data;
 
 	for (uint32_t texture_index = 0; texture_index < scene->texture_count; ++texture_index) {
-		TextureSource *src = &scene->textures[texture_index];
-		Texture *dst = &state.textures[state.texture_count];
+		if (texture_index > MAX_MODEL_TEXTURES)
+			assert("Model has more than max textures");
+		TextureSource *src_texture = &scene->textures[texture_index];
+		Texture *dst_texture = &dst->textures[dst->texture_count++];
 
-		dst->texture = state.texture_count++;
-		dst->sampler = 0;
+		dst_texture->texture = state.texture_count++;
+		dst_texture->sampler = state.default_sampler;
 
-		vulkan_renderer_create_texture(state.ctx, dst->texture, src->width, src->height, src->channels, src->pixels);
+		vulkan_renderer_create_texture(state.ctx, dst_texture->texture, src_texture->width, src_texture->height, src_texture->channels, src_texture->pixels);
 	}
 
 	for (uint32_t material_index = 0; material_index < scene->material_count; ++material_index) {
-		MaterialSource *src = &scene->materials[material_index];
-		MaterialInstance *dst = &state.materials[state.material_count++];
+		if (material_index >= MAX_MODEL_MATERIALS)
+			assert("Model has more than max materials");
 
-		dst->material = state.PBR_material;
-		dst->parameter_uniform_buffer = state.buffer_count++;
-		dst->resource_set = state.set_count++;
+		MaterialSource *src_material = &scene->materials[material_index];
+		MaterialInstance *dst_material = &dst->materials[dst->material_count++];
 
-		vulkan_renderer_create_resource_set(state.ctx, dst->resource_set, state.PBR_material.shader, SHADER_UNIFORM_FREQUENCY_PER_MATERIAL);
+		dst_material->material = state.PBR_material;
+		dst_material->parameter_uniform_buffer = state.buffer_count++;
+		dst_material->resource_set = state.set_count++;
+
+		vulkan_renderer_create_resource_set(state.ctx, dst_material->resource_set, state.PBR_material.shader, SHADER_UNIFORM_FREQUENCY_PER_MATERIAL);
 
 		MaterialParameters parameters = {
 			.base_color_factor = {
-			  src->base_color_factor[0],
-			  src->base_color_factor[1],
-			  src->base_color_factor[2],
-			  src->base_color_factor[3],
+			  src_material->base_color_factor[0],
+			  src_material->base_color_factor[1],
+			  src_material->base_color_factor[2],
+			  src_material->base_color_factor[3],
 			},
-			.metallic_factor = src->metallic_factor,
-			.roughness_factor = src->roughness_factor,
+			.metallic_factor = src_material->metallic_factor,
+			.roughness_factor = src_material->roughness_factor,
 			.emissive_factor = {
-			  src->emissive_factor[0],
-			  src->emissive_factor[1],
-			  src->emissive_factor[2],
+			  src_material->emissive_factor[0],
+			  src_material->emissive_factor[1],
+			  src_material->emissive_factor[2],
 			}
 		};
 
-		vulkan_renderer_create_buffer(state.ctx, dst->parameter_uniform_buffer, BUFFER_TYPE_UNIFORM, sizeof(MaterialParameters), &parameters);
-		vulkan_renderer_update_resource_set_buffer(state.ctx, dst->resource_set, "u_material_parameters", dst->parameter_uniform_buffer);
+		vulkan_renderer_create_buffer(state.ctx, dst_material->parameter_uniform_buffer, BUFFER_TYPE_UNIFORM, sizeof(MaterialParameters), &parameters);
+		vulkan_renderer_update_resource_set_buffer(state.ctx, dst_material->resource_set, "u_material_parameters", dst_material->parameter_uniform_buffer);
 
-		if (src->base_color_texture) {
-			uint32_t texture_index = src->base_color_texture - scene->textures;
-			texture_index += state.default_texture_offset;
-			vulkan_renderer_update_resource_set_texture_sampler(state.ctx, dst->resource_set, "u_base_color_texture", texture_index, state.default_sampler);
+		if (src_material->base_color_texture) {
+			uint32_t texture_index = src_material->base_color_texture - scene->textures;
+			texture_index += texture_offset;
+			vulkan_renderer_update_resource_set_texture_sampler(state.ctx, dst_material->resource_set, "u_base_color_texture", texture_index, state.default_sampler);
 		} else {
-			vulkan_renderer_update_resource_set_texture_sampler(state.ctx, dst->resource_set, "u_base_color_texture", state.default_texture_base_color, state.default_sampler);
+			vulkan_renderer_update_resource_set_texture_sampler(state.ctx, dst_material->resource_set, "u_base_color_texture", state.default_texture_base_color, state.default_sampler);
 		}
 	}
 
 	for (uint32_t mesh_index = 0; mesh_index < scene->mesh_count; ++mesh_index) {
-		MeshSource *src = &scene->meshes[mesh_index];
-		Mesh *dst = &state.meshes[state.mesh_count++];
+		if (mesh_index > MAX_MODEL_MESHES)
+			assert("Model has more than max meshes");
 
-		dst->vertex_buffer = state.buffer_count++;
-		dst->index_buffer = state.buffer_count++;
+		MeshSource *src_mesh = &scene->meshes[mesh_index];
+		Mesh *dst_mesh = &dst->meshes[dst->mesh_count++];
 
-		dst->vertex_count = src->vertex_count;
-		dst->index_count = src->index_count;
+		dst_mesh->vertex_buffer = state.buffer_count++;
+		dst_mesh->index_buffer = state.buffer_count++;
 
-		dst->material = src->material - scene->materials;
+		dst_mesh->vertex_count = src_mesh->vertex_count;
+		dst_mesh->index_count = src_mesh->index_count;
 
-		vulkan_renderer_create_buffer(state.ctx, dst->vertex_buffer, BUFFER_TYPE_VERTEX, sizeof(Vertex) * src->vertex_count, src->vertices);
-		vulkan_renderer_create_buffer(state.ctx, dst->index_buffer, BUFFER_TYPE_INDEX, sizeof(uint32_t) * src->index_count, src->indices);
+		dst_mesh->material = src_mesh->material - scene->materials;
+
+		vulkan_renderer_create_buffer(state.ctx, dst_mesh->vertex_buffer, BUFFER_TYPE_VERTEX, sizeof(Vertex) * src_mesh->vertex_count, src_mesh->vertices);
+		vulkan_renderer_create_buffer(state.ctx, dst_mesh->index_buffer, BUFFER_TYPE_INDEX, sizeof(uint32_t) * src_mesh->index_count, src_mesh->indices);
 	}
 
 	return true;
